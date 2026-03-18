58578775d0aac7357059732269ffd213b17f0cfa
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-03-18 22:29:07 +0545

    feat: integrate Tailwind CSS and add SquareBox component
    - Added Tailwind CSS and its Vite plugin to dependencies.
    - Updated App component to include a title and multiple SquareBox components with different colors.
    - Removed unused assets: hero.png, react.svg, and vite.svg.
    - Created a new SquareBox component for rendering colored square boxes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { SquareBox } from "./components/SquareBox";
 
 const App = () => {
-  return <div className="bg-amber-400">App</div>;
+  return (
+    <div className=" p-4 flex flex-row gap-4">
+      <h1 className="text-2xl font-bold text-black">My App</h1>
+      <SquareBox color="red" />
+      <SquareBox color="blue" />
+      <SquareBox color="green" />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/SquareBox.jsx
@@ -0,0 +1,3 @@
+export const SquareBox = ({ color }) => {
+  return <div className="w-20 h-20" style={{ backgroundColor: color }} />;
+};
